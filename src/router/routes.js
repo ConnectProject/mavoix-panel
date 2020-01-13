a785e3940d73c4887b30ea4c12e371ca735fd768
Pierre Green <pierregreen@protonmail.com>
2020-01-13 19:00:16 +0100

    Route doc

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,10 @@
 
 const routes = [
-  /* Logged routes */
   {
     path: '/auth',
     component: () => import('~/layouts/AuthLayout.vue'),
     children: [
+      /* Auth homepage */
       { name: 'auth', path: '/', component: () => import('~/pages/Authenticate.vue') }
     ]
   },
@@ -13,9 +13,10 @@ const routes = [
     path: '/:assets?',
     component: () => import('~/layouts/HomeLayout.vue'),
     children: [
+      /* Homepage */
       { name: 'home', path: '/home', component: () => import('~/pages/Index.vue') },
 
-      /* Tabs routes */
+      /* Tab editor */
       { name: 'tab', path: '/tabs/:slug', component: () => import('~/pages/TabEditor.vue') },
 
       /* Devices routes */
