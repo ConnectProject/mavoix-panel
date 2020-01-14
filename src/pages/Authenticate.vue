8fdaefbe3ab8cf77532d179b226ead582a634513
Pierre Green <pierregreen@protonmail.com>
2020-01-14 02:04:50 +0100

    Pages docs

--- src/pages/Authenticate.vue
+++ src/pages/Authenticate.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page class="flex flex-center">
     <q-card>
-      <q-btn @click="$router.push({ name: 'home' })">Connexion avec github</q-btn>
+      <q-btn @click="$router.push({ name: 'home' })">
+        Connexion avec github
+      </q-btn>
     </q-card>
   </q-page>
 </template>
--- src/pages/TabEditor.vue
+++ src/pages/TabEditor.vue
@@ -19,8 +19,13 @@
 </style>
 
 <template>
+  <!-- Print nothing if loading -->
   <div v-if="loading"></div>
-  <q-page v-else class="flex items-stretch q-pa-xl">
+
+  <q-page
+    v-else
+    class="flex items-stretch q-pa-xl"
+  >
     <div class="column container">
       <div class="col-2 row justify-center items-center">
 
@@ -42,39 +47,82 @@
           filled
         >
           <template v-slot:append>
-            <q-icon name="colorize" class="cursor-pointer">
-              <q-popup-proxy transition-show="scale" transition-hide="scale">
-                <q-color no-header no-footer :value="tab.hexColor" @input="setHexColor" inline class="my-picker" />
+            <q-icon
+              name="colorize"
+              class="cursor-pointer"
+            >
+              <q-popup-proxy
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-color
+                  no-header
+                  no-footer
+                  :value="tab.hexColor"
+                  @input="setHexColor"
+                  inline
+                  class="my-picker"
+                />
               </q-popup-proxy>
             </q-icon>
           </template>
         </q-input>
 
         <!-- Add item button -->
-        <q-btn class="stretch q-ma-md" flat size="lg" @click="onAddItem">{{ $t('tabEditor.addItemLabel') }}</q-btn>
+        <q-btn
+          class="stretch q-ma-md"
+          flat size="lg"
+          @click="onAddItem">
+            {{ $t('tabEditor.addItemLabel') }}
+          </q-btn>
       </div>
 
+      <!-- Items container -->
       <draggable
         tag="div"
         v-model="items"
         :style="{ borderColor: tab.hexColor }"
         class="col-8 scroll row items-start rounded-borders q-pa-xs q-ph-md q-mh-xs q-mx-md q-gutter-x-xl q-gutter-y-md items-container">
-        <q-card class="col-2" v-for="(item, index) in items" :v-if="!item.deleted" :key="index">
-          <q-img :ratio="16/9" class="item-img" :src="item.asset.file._url">
-            <div class="absolute-bottom">
-              <div class="text-center text-subtitle2">{{ item.name }}</div>
+
+        <!-- Items -->
+        <q-card
+          class="col-2"
+          v-for="(item, index) in items"
+          :v-if="!item.deleted"
+          :key="index"
+        >
+          <!-- Item's image -->
+          <q-img
+            :ratio="16/9"
+            class="item-img"
+            :src="item.asset.file._url"
+          >
+            <div
+              class="absolute-bottom"
+            >
+              <!-- Item's name -->
+              <div class="text-center text-subtitle2">
+                {{ item.name }}
+              </div>
             </div>
-            <div @click="() => onEditItem(item, index)" class="absolute fit flex justify-center items-center text-center action-icon-wrapper">
+
+            <!-- Edit button -->
+            <div
+              @click="() => onEditItem(item, index)"
+              class="absolute fit flex justify-center items-center text-center action-icon-wrapper"
+            >
               <q-icon name="edit" class="action-icon" size="xl" />
             </div>
           </q-img>
 
           <q-card-actions align="right">
+            <!-- Toggle availability -->
             <q-checkbox
               left-label
               :label="$t('generic.unavailable')"
               :value="!item.available"
               @input="() => $store.commit('tabEditor/toggleItemAvailable', item)"/>
+            <!-- Toggle visibility -->
             <q-checkbox
               left-label
               :label="$t('generic.invisible')"
@@ -123,15 +171,30 @@ import { SLUG_KEY } from '../models/Tab'
 
 export default {
   name: 'TabEditor',
+  components: {
+    DialogTabItem
+  },
+  /**
+   * When component mounted load everything
+   */
+  mounted () {
+    this.load()
+  },
   data () {
     return {
       hex: ''
     }
   },
   computed: {
+    /**
+     * Return the opened tab
+     */
     tab () {
       return this.$store.getters['tabEditor/tab']
     },
+    /**
+     * The tab's items
+     */
     items: {
       get () {
         return this.$store.getters['tabEditor/items']
@@ -140,19 +203,80 @@ export default {
         this.$store.commit('tabEditor/setItemsRaw', values)
       }
     },
+    /**
+     * Return true if loading
+     */
     loading () {
       return this.$store.getters['tabEditor/loading']
     }
   },
   watch: {
+    /**
+     * When route change load eveyrhing
+     */
     '$route' (newValue, oldValue) {
       this.load()
     }
   },
   methods: {
+    /**
+     * Load tab
+     */
+    load () {
+      this.$store.dispatch('tabEditor/loadBySlug', this.$route.params.slug)
+    },
+    /**
+     * Call to save the tab
+     */
+    onSave () {
+      this.$store.dispatch('tabEditor/saveCb', (tab) => {
+        /* When the tab's name is changed the slugs change to, so redirect to the new url */
+        this.$router.push({
+          name: 'tab',
+          params: {
+            slug: tab.get(SLUG_KEY)
+          }
+        })
+
+        /* Toast message */
+        this.$q.notify({
+          message: `${tab.get('name')} saved`,
+          color: 'purple'
+        })
+      })
+    },
+    /**
+     * Call to remove tab
+     */
+    onDelete () {
+      this.$store.dispatch('tabEditor/deleteTab')
+
+      /* Go to home */
+      this.$router.push({
+        name: 'home'
+      })
+    },
+    /**
+     * Set the tab's name
+     */
+    setName (name) {
+      this.$store.commit('tabEditor/setName', name)
+    },
+    /**
+     * Set the tab's color
+     */
+    setHexColor (hexColor) {
+      this.$store.commit('tabEditor/setHexColor', hexColor)
+    },
+    /**
+     * Call to open the item dialog in new item mode
+     */
     onAddItem () {
       this.$store.commit('tabEditor/openItemDialog', {})
     },
+    /**
+     * Call to open the item dialog in edit item mode
+     */
     onEditItem (item, index) {
       this.$store.commit('tabEditor/openItemDialog', {
         mode: 'edit',
@@ -160,47 +284,18 @@ export default {
         data: Object.assign({}, item)
       })
     },
-    onSave () {
-      this.$store.dispatch('tabEditor/saveCb', (tab) => {
-        this.$router.push({
-          name: 'tab',
-          params: {
-            slug: tab.get(SLUG_KEY)
-          }
-        })
-        this.$q.notify({
-          message: `${tab.get('name')} saved`,
-          color: 'purple'
-        })
-      })
-    },
-    onDelete () {
-      this.$store.dispatch('tabEditor/deleteTab')
-      this.$router.push({
-        name: 'home'
-      })
-    },
-    setName (name) {
-      this.$store.commit('tabEditor/setName', name)
-    },
-    setHexColor (hexColor) {
-      this.$store.commit('tabEditor/setHexColor', hexColor)
-    },
+    /**
+     * Undo
+     */
     onUndo () {
       this.$store.commit('tabEditor/undo')
     },
+    /**
+     * Redo
+     */
     onRedo () {
       this.$store.commit('tabEditor/redo')
-    },
-    load () {
-      this.$store.dispatch('tabEditor/loadBySlug', this.$route.params.slug)
     }
-  },
-  mounted () {
-    this.load()
-  },
-  components: {
-    DialogTabItem
   }
 }
 </script>
