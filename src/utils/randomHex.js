ce3e2b2d51098029e47ef5cb6dd99aa07c23a1b3
r03ert0 <rto@braincatalogue.org>
2020-12-29 16:21:51 +0100

    dimmer random colors

--- src/utils/randomHex.js
+++ src/utils/randomHex.js
@@ -1,6 +1,23 @@
 /**
  * Generate a random 16 bytes hex string
  */
-export default () => (
-  `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
-)
+
+export default () => {
+  const h = `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
+
+  // change colour saturation, make it darker
+  const rgb = [
+    `0x${h[1] + h[2]}` | 0,
+    `0x${h[3] + h[4]}` | 0,
+    `0x${h[5] + h[6]}` | 0
+  ]
+
+  const h2 = `#${rgb.map((o) => {
+    const saturation = 0.25
+    const x = (255 * (1 - saturation) + o * saturation) | 0
+    const s = ('00' + x.toString(16)).slice(-2)
+    return s
+  }).join('')}`
+
+  return h2
+}
