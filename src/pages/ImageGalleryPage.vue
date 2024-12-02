9d959d788094a03b131605cc1740d14d1aa8252f
Simon Malesys <simon.malesys@pasteur.fr>
2024-12-02 14:59:17 +0100

    Add a filter in the image gallery

--- src/pages/ImageGalleryPage.vue
+++ src/pages/ImageGalleryPage.vue
@@ -6,18 +6,18 @@
 
     <hr>
 
-    <!-- <section class="search-section">
-      <label for="search-images">Search for images :</label>
+    <section class="search-section">
+      <label for="search-images">Cherchez dans vos images :</label>
       <input
         id="search-images"
         type="search"
-      >
-    </section> -->
+        v-model="imageFilter">
+    </section>
 
     <section class="images-section">
       <h2>Explorez vos images : </h2>
       <div class="cards">
-        <div class="image-card" v-for="image in assets">
+        <div class="image-card" v-for="image in filteredImages">
           <img :src="image.url" alt="">
           <div>{{ image.name }}</div>
         </div>
@@ -31,14 +31,42 @@ import Parse from 'parse'
 
 export default {
   name: "ImageGalleryPage",
+  data: function () {
+    return {
+      imageFilter: ''
+    }
+  },
   mounted() {
     if (Parse.User.current()) {
       this.$store.dispatch('assetsManager/loadAssets')
     }
   },
   computed: {
-    assets () {
+    images() {
       return this.$store.getters['assetsManager/all']
+    },
+    filteredImages() {
+      return this.images.filter(image => {
+        const keyword = this.normalizeString(this.imageFilter)
+        const imageName = this.normalizeString(image.name)
+
+        return imageName.includes(keyword)
+      })
+    }
+  },
+  methods: {
+    /**
+     * Make a string lowercase remove diacritics.
+     * It is used to provide a case-insensitive and
+     * diacritic-insensitive filtering for the user.
+     * @param {string} str - The string to normalize
+     * @return {string} The normalized string
+     */
+    normalizeString(str) {
+      return str
+        .normalize("NFD") // Normalize the unicode chain to extract diacritics
+        .replace(/[\u0300-\u036f]/g, "") // Remove diacritic characters
+        .toLowerCase();
     }
   }
 }
